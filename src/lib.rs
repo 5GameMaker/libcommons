c2b2c4bfc67b887e51ebee545f9bc758b7a24692
buj <buj351@outlook.com>
2025-05-13 01:17:41 +0700

    added io piping

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(any(feature = "dirs", feature = "lock"))]
 pub mod fs;
+#[cfg(feature = "io")]
+pub mod io;
 #[cfg(any(feature = "dirs", feature = "lock"))]
 pub mod os;
 #[cfg(feature = "str")]
@@ -8,6 +10,8 @@ pub mod str;
 pub mod util;
 
 pub mod prelude {
+    #[cfg(feature = "io")]
+    pub use crate::io::ReadExt;
     #[cfg(feature = "str")]
     pub use crate::str::AsUtf8;
     #[cfg(feature = "extra_traits")]
